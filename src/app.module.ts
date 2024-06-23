e2379ce3a968c99644f58a687e685fec3b6ccc22
waldiii <waldocu02@gmail.com>
2024-06-23 01:39:18 -0400

    first steps

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { StudentModule } from './student/student.module';
+import { SubjectModule } from './subject/subject.module';
+import { EvaluationModule } from './evaluation/evaluation.module';
+import { TeacherModule } from './teacher/teacher.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [StudentModule, SubjectModule, EvaluationModule, TeacherModule, UserModule],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
