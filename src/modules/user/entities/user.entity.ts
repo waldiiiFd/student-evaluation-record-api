5567c1d3bc8b765dd632f62e12785ed90803c9d4
waldiii <waldocu02@gmail.com>
2024-06-30 23:42:50 -0400

    starting user stuff

--- src/modules/user/entities/user.entity.ts
+++ src/modules/user/entities/user.entity.ts
@@ -1,1 +1,15 @@
-export class User {}
+import { Column, PrimaryGeneratedColumn } from "typeorm";
+
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({unique:true})
+  email: string;
+
+  @Column()
+  password: string;
+
+  @Column()
+  role: number;
+}
